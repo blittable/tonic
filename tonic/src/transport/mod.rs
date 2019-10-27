26b193263ef5e0b6a0cf6084c5acdecd2469441e
blittable <blittable-bits@gmail.com>
2019-10-27 12:24:56 +0700

    Revert "tls feature import for unit test"
    This reverts commit 9a59bfc22f15d7bfd8d610fca51e422a7cd94875.

--- tonic/src/transport/mod.rs
+++ tonic/src/transport/mod.rs
@@ -19,7 +19,6 @@
 //! ## Client
 //!
 //! ```no_run
-//! # # [cfg(feature = "tls")]
 //! # use tonic::transport::{Channel, Certificate, ClientTlsConfig};
 //! # use std::time::Duration;
 //! # use tonic::body::BoxBody;
@@ -46,7 +45,6 @@
 //! ## Server
 //!
 //! ```no_run
-//! # #[cfg(feature = "tls")]
 //! # use tonic::transport::{Server, Identity, ServerTlsConfig};
 //! # use tower::{Service, service_fn};
 //! # use futures_util::future::{err, ok};
